cdf094fa28a53c646f5cc9623317ac0f97c72e0c
Nahid Hasan Sagar <nahidhasansagar@gmail.com>
2025-10-15 11:41:35 -0400

    simulate_reads.R modified for nonzero entries

--- STAT-S 610 Homework 6/simulate_reads.R
+++ STAT-S 610 Homework 6/simulate_reads.R
@@ -1,14 +1,21 @@
 #' @param n_snps The length of the sequence
 #' @param n_true_seqs The number of sequences
 #' @return A matrix of 0's and 1's with number of rows equal to n_snps and number of columns equal to n_true_seqs
-make_true_seqs = function(n_snps, n_true_seqs) {
+make_true_seqs = function(n_snps, n_true_seqs, n_nonzero = NULL) {
   true_seqs = matrix(NA, nrow = n_true_seqs, ncol = n_snps)
   for(i in 1:n_snps) {
-    snp_vals = sample(0:1, size = n_true_seqs, replace = TRUE)
-    if(all(snp_vals == 0)) {
-      snp_vals[sample(n_true_seqs, 1)] = 1
-    } else if(all(snp_vals == 1)) {
-      snp_vals[sample(n_true_seqs, 1)] = 0
+    if(!is.null(n_nonzero)) {
+      # The specified number of nonzero entries
+      snp_vals = rep(0, n_true_seqs)
+      nonzero_indices = sample(n_true_seqs, n_nonzero)
+      snp_vals[nonzero_indices] = 1
+    } else{
+      snp_vals = sample(0:1, size = n_true_seqs, replace = TRUE)
+      if(all(snp_vals == 0)) {
+        snp_vals[sample(n_true_seqs, 1)] = 1
+      } else if(all(snp_vals == 1)) {
+        snp_vals[sample(n_true_seqs, 1)] = 0
+      }
     }
     true_seqs[,i] = snp_vals
   }
